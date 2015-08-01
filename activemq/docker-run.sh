5cb216f69019a448777144e9caa598687dce333c
Matthias Bertram <matt@web-dev.space>
2015-08-01 18:35:22 +1200

    Expose STOMP, WebSocket, and MQTT connectors

--- activemq/docker-run.sh
+++ activemq/docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name=activemq -p=61616:61616 -p=8161:8161 -d mbertram/activemq
+docker run --name=activemq -p=61616:61616 -p=8161:8161 -p=61613:61613 -p=1883:1883 -p=61614:61614 -d mbertram/activemq
